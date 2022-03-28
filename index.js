c539bb9dffd54ef369dd239f1353aed395a1e5c0
Loyda <palacioloydita@gmail.com>
2022-03-28 13:30:05 -0700

    commit tree 67303c8565a76bd87ea310a1133c205606ba75f5

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution here
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+function findMatching(drivers, string) {
+    const result = drivers.filter(driversName => driversName.toLowerCase() === string.toLowerCase())
+        return result;
+}
+
+function fuzzyMatch(drivers, string) {
+    const result = drivers.filter(driversName => driversName.startsWith(string))
+        return result;
+}   
+
+function matchName(drivers, string) {
+    const result  = drivers.filter(drivers => drivers.name === string)
+        return result;
+}
